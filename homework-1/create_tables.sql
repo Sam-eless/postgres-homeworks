7f398f4fecebbe1c9adef7cb0402dcd0f5690d50
Sam <iaiznura@gmail.com>
2023-04-06 21:50:38 +0300

    half done homework-1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,29 @@
--- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	first_name varchar(100),
+	last_name varchar(100),
+	title varchar(100),
+	birth_date varchar(100),
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(100) PRIMARY KEY,
+	company_name varchar(100),
+	contact_name varchar(100)
+
+);
+
+CREATE TABLE orders
+(
+	order_id varchar(100) PRIMARY KEY,
+	customer_id varchar(100) REFERENCES customers(customer_id) NOT NULL,
+	employee_id varchar(100),
+	order_date varchar(100),
+	ship_city varchar(100)
+);
+
+SELECT * FROM employees;
+SELECT * FROM customers;
+SELECT * FROM orders;
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,34 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+
+
+def main():
+    conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='123131')
+    cur = conn.cursor()
+    customers_data = 'north_data/customers_data.csv'
+    employees_data = 'north_data/employees_data.csv'
+    orders_data = 'north_data/orders_data.csv'
+
+    try:
+        sql = "COPY %s FROM STDIN WITH CSV HEADER DELIMITER AS ','"
+        e = open(employees_data, 'r', encoding='utf8')
+        cur.execute("truncate " + 'employees' + ";")
+        cur.copy_expert(sql=sql % 'employees', file=e)
+
+        # with open(employees_data, newline='') as file:
+        #     cur.copy_from(file, 'employees', sep=',')
+
+        with open(customers_data, newline='') as file:
+            cur.copy_from(file, 'customers', sep=',')
+
+        with open(orders_data, newline='') as file:
+            cur.copy_from(file, 'orders', sep=',')
+
+    finally:
+        conn.commit()
+        cur.close()
+        conn.close()
+
+
+if __name__ == '__main__':
+    main()
